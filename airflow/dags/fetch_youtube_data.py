0350352b9d4bbe25bd6206e411b6eb9df43ce09f
Lizzzzzz <34636027+LizLicense@users.noreply.github.com>
2025-03-17 21:20:21 +0800

    Update fetch_youtube_data.py

--- airflow/dags/fetch_youtube_data.py
+++ airflow/dags/fetch_youtube_data.py
@@ -85,7 +85,7 @@ def fetch_and_save_youtube_data(**kwargs):
     """
     try:
         # Read topic from file
-        with open('/Users/[username]/.../topic.txt', 'r') as f:
+        with open('/Users/[filename]/.../topic.txt', 'r') as f:
             topic = f.read().strip()
         
         print(f"Successfully read topic: {topic}")
@@ -94,7 +94,7 @@ def fetch_and_save_youtube_data(**kwargs):
         videos_data = get_youtube_data(topic)
         
         # Save to JSON file
-        output_file = f'/Users/[username]/.../{topic}.json'
+        output_file = f'/Users/[filename]/.../{topic}.json'
         with open(output_file, 'w') as f:
             json.dump(videos_data, f, indent=2)
         
